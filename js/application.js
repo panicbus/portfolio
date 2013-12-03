3061664167fe9493f7d1eb9b868f41e75b26d329
Nico Crisafulli <elementalair@gmail.com>
2013-12-03 13:01:18 -0800

    initial commit of all files

---
+++ js/application.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("img").hover(function() {
+      $(this).stop().animate({opacity: "0.8"}, 'slow');
+    },
+    function() {
+      $(this).stop().animate({opacity: "1"}, 'slow');
+    });
+  });
+
+$(document).ready(function(){
+  $("#hamburger").click(function(){
+    $( "#the_more_div").slideToggle('slow');
+  });
+});
+
+
+// $(document).ready ->
+//   $(".main").css "display", "none"
+//   $(".main").fadeIn(500)
+
