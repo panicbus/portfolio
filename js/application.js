a4dd96a022c406eab63a000816f7cfb3a021f39b
Nico Crisafulli <elementalair@gmail.com>
2014-01-08 15:01:31 -0800

    changed to single page

--- js/application.js
+++ js/application.js
@@ -1,3 +1,21 @@
+var _hidediv = null;
+var previousDiv = null;
+function showdiv(id) {
+   if(_hidediv != null){
+    _hidediv();
+  }
+
+var div = document.getElementById(id);
+  if(div != previousDiv) {
+    div.style.display = 'block';
+    _hidediv = function() { div.style.display = 'none'; };
+    previousDiv = div;
+  } else {
+    previousDiv = null;
+  }
+}
+
+
 $(document).ready(function(){
     $("img").hover(function() {
       $(this).stop().animate({opacity: "0.8"}, 'slow');
@@ -5,7 +23,7 @@ $(document).ready(function(){
     function() {
       $(this).stop().animate({opacity: "1"}, 'slow');
     });
-  });
+}); // ends document ready function
 
 $(document).ready(function(){
   $("#hamburger").click(function(){
@@ -14,17 +32,12 @@ $(document).ready(function(){
 });
 
 
-$(function(){
-  var menuItems = $('.links li').on('click', function(){
-    console.log(menuItems);
-    menuItems.removeClass('current');
-    $(this).addClass('current');
-    console.log(this);
-  });
-});
-
-
-// $(document).ready ->
-//   $(".main").css "display", "none"
-//   $(".main").fadeIn(500)
+// $(function(){
+//   var menuItems = $('.links li').on('click', function(){
+//     console.log(menuItems);
+//     menuItems.removeClass('current');
+//     $(this).addClass('current');
+//     console.log(this);
+//   });
+// });
 
