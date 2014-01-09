9a9c3a9d22e041fbfd8db15c2ab59ee97aaf1d14
Nico Crisafulli <elementalair@gmail.com>
2014-01-08 23:12:34 -0800

    media query style finalization

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var colorLink = function(link) {
+  var hoverLink = function(link) {
     $('.view-options').removeClass('current');
     $(link).addClass('current');
   }
@@ -11,45 +11,38 @@ $(document).ready(function(){
   $('body').on('click',".about", function(event) {
     $('.nico-view').hide()
     $('#about').show()
-    colorLink(this)
+    hoverLink(this)
   })
 
   $('body').on('click',".work", function(event) {
     $('.nico-view').hide()
     $('#work').show()
-    colorLink(this)
+    hoverLink(this)
   })
 
   $('body').on('click',".photos", function(event) {
     $('.nico-view').hide()
     $('#photos').show()
-    colorLink(this)
+    hoverLink(this)
   })
 
   $('body').on('click',".splash", function(event) {
     $('.nico-view').hide();
     $('#splash').show();
-    colorLink(this).hide();
+    hoverLink(this).hide();
   })
 
- // function showDiv(view) {
- //    $('.nico-view').hide()
- //    $('#' + view).show()
- //  }
 
- //  showDiv('splash');
- //  $('body').on('click', '.about', showDiv('about'))
- //  $('body').on('click', '.work',  showDiv('work'))
- //  $('body').on('click', '.photos',showDiv('photos'))
+  // image hover effect
+  $("img").hover(function(){
+    $(this).stop().animate({opacity: "0.8"}, 'slow');
+  },
+  function() {
+    $(this).stop().animate({opacity: "1"}, 'slow');
+  });
 
 
-    $("img").hover(function(){
-      $(this).stop().animate({opacity: "0.8"}, 'slow');
-    },
-    function() {
-      $(this).stop().animate({opacity: "1"}, 'slow');
-    });
-
+  // sidebar slide toggle
   $("#hamburger").click(function(){
     $( "#the_more_div").slideToggle('slow');
   });
