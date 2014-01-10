8e8632d57d9b20220f2a632cee1affa423d1e318
Nico Crisafulli <elementalair@gmail.com>
2014-01-10 12:01:32 -0800

    major redesign of site

--- js/application.js
+++ js/application.js
@@ -9,37 +9,43 @@ $(document).ready(function(){
   $('#splash').show()
 
   $('body').on('click',".about", function(event) {
-    $('.nico-view').hide()
-    $('#about').show()
-    hoverLink(this)
+    $('.nico-view').hide('slow');
+    $('#about').slideDown('slow');
+    hoverLink(this);
   })
 
   $('body').on('click',".work", function(event) {
-    $('.nico-view').hide()
-    $('#work').show()
-    hoverLink(this)
+    $('.nico-view').hide('slow');
+    $('#work').slideDown('slow');
+    hoverLink(this);
   })
 
   $('body').on('click',".photos", function(event) {
-    $('.nico-view').hide()
-    $('#photos').show()
+    $('.nico-view').hide('slow');
+    $('#photos').slideDown('slow');
     hoverLink(this)
   })
 
-  $('body').on('click',".splash", function(event) {
-    $('.nico-view').hide();
-    $('#splash').show();
+  $('body').on('click',".resume", function(event) {
+    $('.nico-view').hide('slow');
+    $('#resume').slideDown('slow');
+    hoverLink(this).hide();
+  })
+
+   $('body').on('click',".splash", function(event) {
+    $('.nico-view').hide('slow');
+    $('#splash').slideDown('slow');
     hoverLink(this).hide();
   })
 
 
   // image hover effect
-  $("img").hover(function(){
-    $(this).stop().animate({opacity: "0.8"}, 'slow');
-  },
-  function() {
-    $(this).stop().animate({opacity: "1"}, 'slow');
-  });
+  // $("img").hover(function(){
+  //   $(this).stop().animate({opacity: "0.8"}, 'slow');
+  // },
+  // function() {
+  //   $(this).stop().animate({opacity: "1"}, 'slow');
+  // });
 
 
   // sidebar slide toggle
