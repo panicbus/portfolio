2e8064f0cad6c2fcc775de061ba27bc005857e75
Nico Crisafulli <elementalair@gmail.com>
2014-01-11 23:42:34 -0800

    sweet photos page lightbox modal update

--- js/application.js
+++ js/application.js
@@ -40,14 +40,35 @@ $(document).ready(function(){
   })
 
 
-  // image hover effect
-  // $("img").hover(function(){
-  //   $(this).stop().animate({opacity: "0.8"}, 'slow');
-  // },
-  // function() {
-  //   $(this).stop().animate({opacity: "1"}, 'slow');
-  // });
+  $('.zoom-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+      verticalFit: true,
+      titleSrc: function(item) {
+        return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+      }
+    },
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function(element) {
+        return element.find('img');
+      }
+    }
+
+  });
+
+
 
 
 });
+
+
 
