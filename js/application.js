637c808613087631e363a5bd1624c33a4825a976
panicbus <elementalair@gmail.com>
2014-04-15 12:52:51 -0700

    the design changes per Octavian Drula

--- js/application.js
+++ js/application.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     $(link).addClass('current');
   }
 
-  // nico-view is the content div
-  // $('.nico-view').hide()
-  // $('#splash').show()
+  // default to #work
+  $('#work-link').addClass('current');
+  $('.nico-view').hide();
+  $('#work').show();
 
+  // nico-view = content div
   $('body').on('click',".about", function(event) {
     $('.nico-view').hide();
     $('#about').show();
@@ -21,32 +23,36 @@ $(document).ready(function(){
     hoverLink(this);
   })
 
-  $('body').on('click',".writing", function(event) {
-    $('.nico-view').hide();
-    $('#writing').show();
-    hoverLink(this);
-  })
-
   $('body').on('click',".resume", function(event) {
     $('.nico-view').hide();
     $('#resume').show();
     hoverLink(this);
   })
 
-   $('body').on('click',".splash", function(event) {
-    $('.nico-view').hide();
-    $('#splash').show();
-    hoverLink(this);
-  })
-
-   // jQuery scrolldown in Work
-  $('a').click(function(){
-    $('html, body').animate({
-      scrollTop: $( $(this).attr('href') ).offset().top - 115 //scrolls to anchor tag in body minus 115 px
-    }, 'slow');
-    return false;
+  $('.up_arrow').hide();
+  
+  $(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y <= 100) {
+      $('#project_banner').fadeIn();
+    }
+    if (y > 100 && y < 1900) {
+        $('.up_arrow').fadeIn();
+        $('.all_work_wrapper').fadeIn();
+        $('#project_banner').fadeOut();
+    } else {
+        $('.up_arrow').fadeOut(200);
+    }
   });
 
+
+  // jQuery scrolldown in Work
+  $('a').click(function(){
+      $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 110 //scrolls to anchor tag in body minus 115 px
+      }, 'slow');
+      return false;
+    });
 
 });
 
